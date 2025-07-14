49b974caaed111970638045103e3a1bd080f639a
Jagan Raajan <jaganraajan@Jagans-MacBook-Air.local>
2025-07-14 14:24:00 -0400

    remove botpress plugin

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,8 +19,6 @@ export default function RootLayout({
     <html lang="en" className="dark:bg-gray-900">
       <head>
         <link rel="icon" href="/favicon.ico" />
-        <script src="https://cdn.botpress.cloud/webchat/v2.3/inject.js" defer></script>
-        <script src="https://files.bpcontent.cloud/2025/04/19/18/20250419181247-GU42H522.js" defer></script>  
       </head>
       <body className={`min-h-screen bg-white dark:bg-gray-900 text-gray-800 dark:text-gray-200`}><StackProvider app={stackServerApp}><StackTheme>
         <Providers>
@@ -32,7 +30,7 @@ export default function RootLayout({
               {children}
             </main>
             <footer className="py-10 text-center text-gray-600 dark:text-gray-400 bg-white dark:bg-gray-800 mt-20 border-t border-lavender-200 dark:border-gray-700">
-              <p>Empowering investors with real-time data and insights. Built with ❤️ by Jagan</p>
+              <p>Empowering investors with real-time data and insights. Built by Jagan</p>
             </footer>
           </div>
         </Providers>
